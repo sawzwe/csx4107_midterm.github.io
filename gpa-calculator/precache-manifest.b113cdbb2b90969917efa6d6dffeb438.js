c75911f5ced7446fcd3aa1c28f5f7172005a06f3
sawzwe <sawjohnwaiyan@gmail.com>
2022-02-03 04:31:56 +0700

    gpa-calculator

---
+++ gpa-calculator/precache-manifest.b113cdbb2b90969917efa6d6dffeb438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665782d1e3d0b1f9cb20551fee321811",
+    "url": "/gpa-calculator/index.html"
+  },
+  {
+    "revision": "112ed3d1d5401c9b6475",
+    "url": "/gpa-calculator/static/css/main.c671d411.chunk.css"
+  },
+  {
+    "revision": "32f0b5e21edef919e0be",
+    "url": "/gpa-calculator/static/js/2.8a64c967.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/gpa-calculator/static/js/2.8a64c967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "112ed3d1d5401c9b6475",
+    "url": "/gpa-calculator/static/js/main.ebdb3021.chunk.js"
+  },
+  {
+    "revision": "d5479effeb6a7b3bd7e9",
+    "url": "/gpa-calculator/static/js/runtime-main.73fd3148.js"
+  }
+]);
